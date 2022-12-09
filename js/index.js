b3a4a51ac6ce6e2d49b604b69899bc555a004081
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2022-12-09 11:28:15 +0300

    feat m3less2 hard plugin try js plugin

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 // * import all modules to index
 
-import {timerControl} from './modules/timer.js';
+import {timerControl, timerPlugin} from './modules/timer.js';
 
 const heroBanner = document.querySelector('.hero__text');
-const heroTimer = document.querySelector('[data-deadline]');
-// const heroTimer = document.querySelector('.hero__timer');
+// const heroTimer = document.querySelector('[data-deadline]');
+const heroTimer = document.querySelector('.hero__timer');
+heroTimer.remove();
 
-timerControl(heroTimer, heroBanner);
+// timerControl(heroTimer, heroBanner);
+timerPlugin();
