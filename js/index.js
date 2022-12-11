a8013aaa8bd5a524bbdd68febf3aa37b9d7f4fd1
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2022-12-11 11:45:33 +0300

    feat m3less2_hard works setTimeout without clearTimeout

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 import {timerControl} from './modules/timer.js';
 
 const heroBanner = document.querySelector('.hero__text');
-const heroTimer = document.querySelector('[data-deadline]');
-// const heroTimer = document.querySelector('.hero__timer');
+const heroTimer = document.querySelector('.hero__timer');
+// const heroTimer = document.querySelector('[data-deadline]');
 
 timerControl(heroTimer, heroBanner);
--- js/modules/timer.js
+++ js/modules/timer.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
-// * this timer is for IndoTravel
 // module for using deadline timer in hero section
+// * this timer is for IndoTravel
 
 import {plural, zeroBeforeNumber} from './utiles.js';
 
@@ -24,58 +24,49 @@ export const timerControl = (timer, banner) => {
   const timerRemove = (elem) => {
     elem.style.display = 'none';
   };
+
   const bannerRemove = (elem) => {
-    elem.style.display = 'none';
     elem.style.opacity = 0;
     elem.innerHTML = '&nbsp;<br>&nbsp;';
   };
 
   // ? parse ? '15/1/2022 21:55' -> '2020-1-15 21:55'
   const deadline = timer.dataset.deadline.split(' ').map(item => item.split('/').reverse().join('-')).join(' ');
-  console.log('deadline: ', deadline);
-
   const timeDeadline = new Date(Date.parse(deadline));
+  console.log('deadline: ', deadline);
+  console.log('timeDeadline: ', timeDeadline);
 
-  const timeout = 10000; // ? изменяем таймаут интервала = 10с для минут
-
-  const timerHandle = (timeDeadline, intervalID, timeout = 1000) => {
-    console.log('timeDeadline inside timerHandle: ', timeDeadline);
-    console.log('timeout inside timerHandle: ', timeout);
+  const timerHandle = (timeDeadline) => {
     const timeNow = new Date();
     const delta = timeDeadline.getTime() - timeNow.getTime();
     if (delta > 0) {
-      console.log('До дедлайна еще осталось ', delta, 'ms');
+      console.log('До дедлайна осталось ', delta, 'ms');
 
-      const timeLeft = parseInt((timeDeadline.getTime() - timeNow.getTime()) / 1000); // secondsAll
+      const timeLeft = parseInt((timeDeadline.getTime() - timeNow.getTime()) / 1000); // all seconds
       const secondsLeft = parseInt(timeLeft % 60);
       const minutesLeft = parseInt(timeLeft / 60 % 60);
       const hoursLeft = parseInt(timeLeft / 60 / 60 % 24);
-      const hoursLeftAll = timeLeft / 60 / 60;
+      const hoursLeftAll = timeLeft / 60 / 60; // float all hours
       const daysLeft = parseInt(timeLeft / 60 / 60 / 24);
 
-      console.log('timeLeft: ', timeLeft);
-      console.log('secondsLeft: ', secondsLeft);
-      console.log('minutesLeft: ', minutesLeft);
-      console.log('hoursLeft: ', hoursLeft);
-      console.log('daysLeft: ', daysLeft);
-
-      console.log('secondTimeout: ', intervalID);
-      console.log('hoursLeftAll: ', hoursLeftAll);
+      console.log('all seconds: ', timeLeft);
+      console.log('secondsLeft: ', secondsLeft,
+        'minutesLeft: ', minutesLeft,
+        'hoursLeft: ', hoursLeft,
+        'daysLeft: ', daysLeft);
 
       if (hoursLeftAll >= 24) {
-        console.log('больше суток в запасе');
+        console.log('больше суток в запасе', hoursLeftAll);
         timerItemDays.style.display = 'flex';
         timerCountDays.textContent = zeroBeforeNumber(daysLeft);
         timerUnitsDays.textContent = plural(daysLeft, ['день', 'дня', 'дней']);
-        timerItemSeconds.style.display = 'none';
-        timeout = 10000;
+        timerItemSeconds.style.display = 'none'; // скрываем секунды
       } else {
-        console.warn('меньше суток');
+        console.log('меньше суток до', deadline);
         timerItemSeconds.style.display = 'flex';
         timerCountSeconds.textContent = zeroBeforeNumber(secondsLeft);
         timerUnitsSeconds.textContent = plural(secondsLeft, ['секунда', 'секунды', 'секунд']);
-        timerItemDays.style.display = 'none';
-        timeout = 1000;
+        timerItemDays.style.display = 'none'; // скрываем дни
       }
 
       timerCountMinutes.textContent = zeroBeforeNumber(minutesLeft);
@@ -84,22 +75,28 @@ export const timerControl = (timer, banner) => {
       timerCountHours.textContent = zeroBeforeNumber(hoursLeft);
       timerUnitsHours.textContent = plural(hoursLeft, ['час', 'часа', 'часов']);
 
-      // todo
-      setTimeout(timerHandle, timeout, timeDeadline, intervalID, timeout);
+      // запускае функцию с разными интервалами чтобы лишний раз не наглужать
+      if (hoursLeftAll < 25) {
+        console.log('сутки');
+        setTimeout(timerHandle, 1000, timeDeadline);
+      } else if (hoursLeftAll < 48) {
+        console.log('около суток');
+        setTimeout(timerHandle, 5000, timeDeadline);
+      } else {
+        console.log('больше суток');
+        setTimeout(timerHandle, 10000, timeDeadline);
+      }
       return;
     } else {
       console.log('Время истекло, ', deadline + ',', timeDeadline);
       timerRemove(timer);
       bannerRemove(banner);
-      // todo ID
-      clearTimeout(intervalID);
-      console.log('очищен secondTimeout: ', intervalID);
       return;
     }
   };
 
-  // todo timeout, timerID
-  const secondTimeout = setTimeout(() => {
-    timerHandle(timeDeadline, secondTimeout, timeout);
+  // запускаем 1й раз
+  setTimeout(() => {
+    timerHandle(timeDeadline);
   }, 0);
 };
