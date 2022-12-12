21571bc61cfbddcdc9ca66bde44dbe3ce335602c
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2022-12-13 01:16:42 +0300

    feat m3less3 accordion it works with animation

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,25 +1,60 @@
 // * accordion interactive module
 
 export const accordionControl = (accordion) => {
+  // проверка на null
   if (!accordion) {
-    console.log('accordion: ', accordion);
+    console.log('accordion null:', accordion);
     return;
   }
+  // работаем дальше
   const travelItems = accordion.querySelectorAll('.travel__item');
-  const travelItemBtns = accordion.querySelectorAll('.travel__item-title');
-  console.log('travelItems: ', travelItems);
-  console.log('travelItemBtns: ', travelItemBtns);
+  const travelBtns = accordion.querySelectorAll('.travel__item-title');
+  const travelWrappers =
+          accordion.querySelectorAll('.travel__item-text-wrapper');
 
+  // console.log('travelItems: ', travelItems);
+  // console.log('travelBtns: ', travelBtns);
+  // console.log('travelWrappers: ', travelWrappers);
+
+  // initial скрываем все вкладки аккордиона
   travelItems.forEach((item, index) => {
-    console.log();
     item.classList.remove('travel__item_active');
+    item.title = travelBtns[index].textContent +
+            ':\nКлик для дополнительной информации';
+    travelWrappers[index].style.height = 0;
   });
-
-  travelItemBtns.forEach((btn, index) => {
+  // todo можно сделать через делегирование
+  // клик по кнопке заголовка вкладки
+  travelBtns.forEach((btn, i) => {
+    // клик по каждой btn
     btn.addEventListener('click', event => {
       const target = event.target;
-      console.log(target, index);
-      travelItems[index].classList.toggle('travel__item_active');
+      console.log('target: ', target);
+      travelItems.forEach((item, j) => {
+        // if (target === travelBtns[j]) {
+        if (i === j) {
+          if (travelItems[j].classList.contains('travel__item_active')) {
+            travelItems[j].classList.remove('travel__item_active');
+            travelWrappers[j].style.height = 0;
+          } else {
+            travelItems[j].classList.add('travel__item_active');
+            travelWrappers[j].style.height =
+                      travelWrappers[j].scrollHeight + 'px';
+          }
+        } else {
+          travelItems[j].classList.remove('travel__item_active');
+          travelWrappers[j].style.height = 0;
+        }
+      });
+    });
+  });
+
+  travelWrappers.forEach((wrapper, index) => {
+    wrapper.addEventListener('click', event => {
+      // wrapper.parentElement.classList.remove('travel__item_active');
+      travelItems[index].classList.remove('travel__item_active');
+      // wrapper.style.height = 0;
+      travelWrappers[index].style.height = 0;
     });
   });
 
