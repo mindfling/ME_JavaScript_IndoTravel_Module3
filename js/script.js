267be241d2e2a3069a1fdf5938613160adad21dd
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2022-12-20 19:45:34 +0300

    feat m3less2 task1 work with Maks debounce

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+/* eslint-disable max-len */
 'use strict';
 
 // простая функция делает перую букву Заглавной отсальные строчными
-const toCapitalizeString = (str) =>
+const toCapitalizedString = (str) =>
   str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
+
 
 // простая функция создания элемента страницы
 const createElem = (tag, attr = {}, text) => {
@@ -14,16 +16,29 @@ const createElem = (tag, attr = {}, text) => {
   return elem;
 };
 
-
-document.addEventListener('DOMContentLoaded', () => {
-  // DOMContentLoaded после загрузки страницы
+const applyStyleSheet = (str) => {
   // динамически подключаем стили ))
   const head = document.head;
   const style = document.createElement('link');
   style.rel = 'stylesheet';
-  style.href = 'css/darkgreen-style.css';
+  style.href = str;
   head.append(style);
+};
+
+// функция обработчик строки ввода
+// const inputHandler = (input, output) => {
+//   console.log('input: ', input.value, 'output: ', output.textContent);
+//   const text = input.value.split(' ')
+//     .map(word => toCapitalizeString(word)).join(' ');
+//   output.textContent = text;
+// };
+
+document.addEventListener('DOMContentLoaded', () => {
+  // DOMContentLoaded после загрузки страницы
+  applyStyleSheet('./css/darkgreen-style.css');
+  // страница
   const page = document.querySelector('.page');
+  // поле ввода input__text
   const input = createElem('input', {
     className: 'page__input',
     type: 'text',
@@ -31,27 +46,69 @@ document.addEventListener('DOMContentLoaded', () => {
     name: 'page-input-text',
     title: 'Начните вводить текст',
   });
-
-  // элемента параграф с пустым символом
+  // поле вывода output__text параграф с пустым символом
   const paragraph = createElem('p', {
     className: 'page__text',
     innerHTML: '&nbsp;',
   });
-
   page.append(input, paragraph);
   page.input = input;
-  page.paragraph = paragraph;
+  page.output = paragraph;
 
-  // функция обработчик строки ввода
-  const inputHandler = (input, output) => {
-    const text = input.value.split(' ')
-      .map(word => toCapitalizeString(word))
-      .join(' ');
-    output.textContent = text;
+  const showText = () => {
+    console.log('page.input: ', page.input.value, 'page.output: ', page.output.textContent);
+    // при запуске этой функ инпут переносится в вывод
+    page.output.textContent = page.input.value;
   };
 
-  // ПРИ НАЧАЛЕ ВВОДА ТЕКСТА В ПОЛЕ ЗАДЕРЖИВАЕМ ВЫВОД ТЕКСТА НА 300 мс
-  input.addEventListener('input', (event) => {
-    setTimeout(inputHandler, 300, input, paragraph);
-  });
+
+  // * Lets using debounce
+  const debounce = (fn, msec) => {
+    console.log('msec: ', msec, 'fn: ', fn);
+    let last = 0;
+    let lastCallTimer = NaN;
+
+    return (...args) => {
+      console.log('args', ...args);
+      // заменяем
+      const previous = last;
+      // обновляем дату последнего вызова
+      last = Date.now();
+      console.log(previous, last, last - previous, lastCallTimer);
+      if (previous && ((last - previous) <= msec)) {
+        console.log('сбрасываем таймер и откидываем этот вызов');
+        clearTimeout(lastCallTimer);
+      }
+      lastCallTimer = setTimeout(() => {
+        console.log('запуск после таймаута');
+        fn(...args);
+      }, msec);
+    };
+  };
+
+  const debounceInputHandle = debounce(showText, 1000);
+
+  page.input.addEventListener('input', debounceInputHandle);
 });
+
+// ПРИ НАЧАЛЕ ВВОДА ТЕКСТА В ПОЛЕ ЗАДЕРЖИВАЕМ ВЫВОД ТЕКСТА НА 300 мс
+// input.addEventListener('input', event => {
+//   setTimeout(() => {
+//     inputHandler(page.input, page.paragraph);
+//   }, 300);
+// });
+
+// input.addEventListener('input', (event) => {
+//   setTimeout(inputHandler, 300, input, paragraph);
+// });
+
+
+// const debounceInputHandle = debounce(inputHandler, 500);
+// console.log('debounceInputHandle: ', debounceInputHandle);
+
+// input.addEventListener('input', (event) => {
+//   debounceInputHandle(page.input, page.paragraph);
+//   console.log(event);
+// });
+
+// page.input.addEventListener('input', showText);
