24630af6f6a8165a33e6d3b0b577c87a44ea4434
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2022-12-20 20:08:36 +0300

    feat m3less2 task1 by Maks debounce func args

--- js/script.js
+++ js/script.js
@@ -26,24 +26,22 @@ const applyStyleSheet = (str) => {
 };
 
 // функция обработчик строки ввода
-// const inputHandler = (input, output) => {
-//   console.log('input: ', input.value, 'output: ', output.textContent);
-//   const text = input.value.split(' ')
-//     .map(word => toCapitalizeString(word)).join(' ');
-//   output.textContent = text;
-// };
+const inputHandle = (input, output) => {
+  console.log('input: ', input.value, 'output: ', output.textContent);
+  const text = input.value.split(' ')
+    .map(word => toCapitalizeString(word)).join(' ');
+  output.textContent = text;
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   // DOMContentLoaded после загрузки страницы
   applyStyleSheet('./css/darkgreen-style.css');
-  // страница
   const page = document.querySelector('.page');
   // поле ввода input__text
   const input = createElem('input', {
     className: 'page__input',
     type: 'text',
     placeholder: 'Введите текст...',
-    name: 'page-input-text',
     title: 'Начните вводить текст',
   });
   // поле вывода output__text параграф с пустым символом
@@ -56,59 +54,30 @@ document.addEventListener('DOMContentLoaded', () => {
   page.output = paragraph;
 
   const showText = () => {
-    console.log('page.input: ', page.input.value, 'page.output: ', page.output.textContent);
-    // при запуске этой функ инпут переносится в вывод
     page.output.textContent = page.input.value;
   };
 
 
-  // * Lets using debounce
-  const debounce = (fn, msec) => {
-    console.log('msec: ', msec, 'fn: ', fn);
+  // * Lets using debounce pattern
+  const debounceTimer = (fn, msec) => {
     let last = 0;
     let lastCallTimer = NaN;
-
     return (...args) => {
-      console.log('args', ...args);
-      // заменяем
       const previous = last;
-      // обновляем дату последнего вызова
       last = Date.now();
-      console.log(previous, last, last - previous, lastCallTimer);
       if (previous && ((last - previous) <= msec)) {
-        console.log('сбрасываем таймер и откидываем этот вызов');
         clearTimeout(lastCallTimer);
       }
       lastCallTimer = setTimeout(() => {
-        console.log('запуск после таймаута');
         fn(...args);
       }, msec);
     };
   };
 
-  const debounceInputHandle = debounce(showText, 1000);
+  const debounceInputHandle = debounceTimer(showText, 300);
 
-  page.input.addEventListener('input', debounceInputHandle);
+  // page.input.addEventListener('input', debounceInputHandle);
+  page.input.addEventListener('input', (event) => {
+    debounceInputHandle(page.input, page.output);
+  });
 });
-
-// ПРИ НАЧАЛЕ ВВОДА ТЕКСТА В ПОЛЕ ЗАДЕРЖИВАЕМ ВЫВОД ТЕКСТА НА 300 мс
-// input.addEventListener('input', event => {
-//   setTimeout(() => {
-//     inputHandler(page.input, page.paragraph);
-//   }, 300);
-// });
-
-// input.addEventListener('input', (event) => {
-//   setTimeout(inputHandler, 300, input, paragraph);
-// });
-
-
-// const debounceInputHandle = debounce(inputHandler, 500);
-// console.log('debounceInputHandle: ', debounceInputHandle);
-
-// input.addEventListener('input', (event) => {
-//   debounceInputHandle(page.input, page.paragraph);
-//   console.log(event);
-// });
-
-// page.input.addEventListener('input', showText);
