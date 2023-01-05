c846f23f5302e156823079ec290fc41436d4837a
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-01-06 01:19:42 +0300

    feat m3less5 with ru price ru dates formates

--- js/modules/tourReservation.js
+++ js/modules/tourReservation.js
@@ -1,3 +1,6 @@
+// todo !!!!!!
+/* eslint-disable padded-blocks */
+/* eslint-disable no-trailing-spaces */
 /* eslint-disable max-len */
 // * tour and reservation
 import {plural} from './utiles.js';
@@ -11,6 +14,42 @@ const getRuPriceFormat = (num) => {
     currency: 'RUB',
   });
   return ruPriceFormatter.format(num);
+};
+
+const getParsedDates = (dates) => {
+  if (!dates) {
+    console.log('wrong dates');
+    return;
+  }
+  const dateRegexp = /\d{2}\.\d{2}/g;
+  const resDates = dates.match(dateRegexp);
+  console.log('resDates: ', resDates);
+  let firstDay = resDates[0];
+  let lastDay = resDates[1];
+
+  if (firstDay && lastDay) {
+    console.log('GOOD firstDay:', firstDay, 'lastDay:', lastDay);
+    const year = (new Date()).getFullYear();
+    firstDay = `${year} ${firstDay.split('.').reverse().join(' ')}`;
+    lastDay = lastDay.split('.').reverse().join(' ') + ' ' + year;
+    
+    console.log('GOOD firstDay:', firstDay, 'lastDay:', lastDay, year);
+
+    const ruDateOptions = {
+      day: 'numeric',
+      month: 'long',
+    };
+    const dateFormatter = 
+      new Intl.DateTimeFormat('ru', ruDateOptions);
+    dates = `${dateFormatter.format(new Date(firstDay))} - ${dateFormatter.format(new Date(lastDay))}`;
+  } else {
+    console.log('bad firstDay:', firstDay, 'lastDay:', lastDay);
+    dates = 'default';
+    firstDay = undefined;
+    lastDay = undefined;
+  }
+  console.log('dates: ', dates);
+  return dates;
 };
 
 // удаленное получение данных о доступных турах
@@ -55,8 +94,7 @@ export const tourBooking = async () => {
   const reservePrice = reservationForm.querySelector('.reservation__price');
 
   const data = await getDataList(URI);
-  console.log('data: ', data);
-
+  // console.log('data: ', data);
 
   clearSelectList(tourDates);
   clearSelectList(tourPeople);
@@ -78,7 +116,7 @@ export const tourBooking = async () => {
     return option;
   });
   // reservation list
-  const list = data.map((item, index) => {
+  const reservationsList = data.map((item, index) => {
     const option = document.createElement('option');
     option.classList.add('tour__option', 'reservation__option');
     option.value = item.date;
@@ -88,11 +126,8 @@ export const tourBooking = async () => {
     return option;
   });
 
-
-  // tourDates.append(...list);
   addOption(tourDates, '', 'Выберите дату');
   tourDates.append(...toursList);
-
 
   tourDates.addEventListener('change', event => {
     const target = event.target;
@@ -118,51 +153,34 @@ export const tourBooking = async () => {
     console.log('tour people', +tourPeople.value);
   });
 
-
   addOption(reserveDates, '', 'Дата путешествия');
-  reserveDates.append(...list);
+  reserveDates.append(...reservationsList);
 
-  // todo with obj tour = {}
-  let date = '';
+  let dates = '';
   let price = '';
-  let firstDay;
-  let lastDay;
-  const dateRegexpTemplate = /\d\d\.\d\d/g;
+  // const rubleSymbol = '₽';
   reserveDates.addEventListener('change', event => {
     clearList(reservePeople);
+
     const selectedIndex = reserveDates.selectedIndex;
     const listIndex = reserveDates[selectedIndex].dataset.index;
+
     if (listIndex) {
       // индекс из выбраного пункта меню
-      const item = data[listIndex];
+      const tour = data[listIndex];
       // дата из массива списка туров
-      date = item.date;
-      const resDates = date.match(dateRegexpTemplate);
-      console.log('resDates: ', resDates);
-      firstDay = resDates[0];
-      lastDay = resDates[1];
-      if (firstDay && lastDay) {
-        console.log('GOOD firstDay:', firstDay, 'lastDay:', lastDay);
-        firstDay = firstDay.split('.').join(' ');
-      } else {
-        console.log('bad firstDay:', firstDay, 'lastDay:', lastDay);
-        date = item.date;
-        firstDay = undefined;
-        lastDay = undefined;
-      }
-      reserveData.textContent = date;
-
-      price = item.price;
-      // reservePrice.textContent = price + ' ₽';
+      dates = tour.date;
+      price = tour.price;
+      dates = getParsedDates(dates);
+      reserveData.textContent = dates;
       reservePrice.textContent = getRuPriceFormat(price);
       addOption(reservePeople, '', 'Количество человек');
-      const minPeople = item['min-people'];
-      const maxPeople = item['max-people'];
+      const minPeople = tour['min-people'];
+      const maxPeople = tour['max-people'];
       for (let i = minPeople; i <= maxPeople; i++) {
         addOption(reservePeople, i, i + ' ' + plural(i,
           ['человек', 'человека', 'человек']));
       }
-      // console.log(minPeople, maxPeople);
     } else {
       reserveData.textContent = '';
       reservePrice.textContent = '';
@@ -174,8 +192,9 @@ export const tourBooking = async () => {
     // const selectedIndex = target.selectedIndex;
     // const selectedValue = target[selectedIndex].value;
     const selectedValue = target.value;
+
     if (selectedValue) {
-      reserveData.textContent = `${date}, ${selectedValue} ${plural(selectedValue,
+      reserveData.textContent = `${dates}, ${selectedValue} ${plural(selectedValue,
         ['человек', 'человека', 'человек'])}`;
       // reservePrice.textContent = price * selectedValue + ' ₽';
       // const ruPriceFormatter = new Intl.NumberFormat('ru', {
@@ -184,10 +203,12 @@ export const tourBooking = async () => {
       // });
       // reservePrice.textContent = ruPriceFormatter.format(summ);
       const summ = price * selectedValue;
+      // reserveData.textContent = date;
       reservePrice.textContent = getRuPriceFormat(summ);
-      console.log(reserveData.textContent, reservePrice.textContent)
+      console.log(reserveData.textContent, reservePrice.textContent);
     } else {
-      reserveData.textContent = date;
+      console.log('date без Количества человек', dates);
+      reserveData.textContent = dates;
       reservePrice.textContent = getRuPriceFormat(price);
     }
   });
